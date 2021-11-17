b0420184424af22ce6d1e2c0a846219ac785b936
sbgreenw <60156138+sbgreenw@users.noreply.github.com>
2021-11-17 14:15:10 -0600

    Documentation + Dict setup changes
    Added documentation, also updated the dictionary setup so that the entries will be ranked in order of their score.

--- BasedBot.py
+++ BasedBot.py
@@ -12,15 +12,18 @@ intents.presences = False
 load_dotenv()
 client = discord.Client()
 
+
+#Both of these can be changed to your liking. For reference, \U0001f60e is the :sunglasses: emote, and \U0001f62c is the :grimacing" emote.
 BASED_EMOJI = '\U0001f60e'
 CRINGE_EMOJI = '\U0001f62c'
 
 
-
+#Write the shitlist to the data.json file
 def flush():
     with open("data.json", "w") as write_file:
         json.dump(d, write_file)
 
+#Load the shitlist from the data.json file. If a file doesn't exist, this means no shitlist, so one must be created.
 def load():
     if(os.path.exists("data.json")):
         with open("data.json", "r") as read_file:
@@ -28,36 +31,54 @@ def load():
     else:
         f = open("data.json", "x")
         f.close()
-        return None
+        return {}
 
 #Takes User as a discord User object
 def shitlist(user, isBased):
     key = user.id
+
+    #If user doesn't have a score yet, their vote count is exactly 1,0 or 0,1 depending on the vote being processed.
     if(not key in d):
         d[key] = [1,0,user.name] if isBased else [0,1,user.name]
+
+    #Otherwise, add one to the correct vote count. The ternary statement is kind of bulky here, but I felt it was less clunky than doing a full if else.
     else:
         d[key] = [d[key][0] + 1, d[key][1], user.name] if isBased else [d[key][0], d[key][1] + 1, user.name]
+
+    #Oh boy, this is a doozy right here. The idea is that dictionaries retain insertion order. So, when this line is ran,
+    #we copy the shitlist (d) into a dictionary which is sorted by social credit score. I'll parse this line to make it easier to understand.
+    #The curly bracket notation makes a dictionary, and populates it with the values for 'uid' and 'entry', which are the returns of a for each loop.
+    #Said foreach loop iterates over every index of dict.items(), which returns a list of the dictionary entries as a (key, value) tuple.
+    #This dict.items() list is sorted by python's built in sorted() function, which takes the list to be ordered, a key to sort by (in this case, social credit,
+    #which is found by taking the first index of the value side of the items() tuple -- item[1][0] -- and subtracting the second index -- item[1][1] from it.
+    #Finally, the list is ordered in reverse (or descending) order. Then, we set d to the copy we made. Dear lord.
+    d = {uid: entry for uid, entry in sorted(d.items(), key = lambda item: item[1][0] - item[1][1], reverse = True)}
+    
+    #Flush the buffer to write the changes to the JSON, so no votes are lost if the program unexpectedly quits.
     flush()
 
 #Takes uid as mention id
 def getScore(uid):
     if(uid not in d):
-        return [-1,-1,-1,-1] #Failure Code
+        return [-1,-1,-1,-1] #Failure Code, returned so the bot can say the user's score doesn't exist.
     basedNum = d[uid][0]
     cringeNum = d[uid][1]
     username = d[uid][2]
     score = basedNum - cringeNum
     return [score, basedNum, cringeNum, username]
 
-#takes user as a mention id
+#Takes user id, returns the formatted string to be posted, containing the user's name, score, and both vote numbers
 def printBased(uid):
     score = getScore(uid)
+    
+    #This only happens if the user doesn't have a score.
     if(score == [-1,-1,-1,-1]):
         return 'The user in question does not currently have a social credit score.'
+        
     return ('The user {0} has a social credit score of {1}, with a number of based votes totaling {2} and a number of cringe votes totaling {3}!').format(score[3], score[0], score[1], score[2])
 
 def printTop():
-    pass
+    return
 
 def printRank():
     pass
